9991dceba620fabeee38dc4e8a5be115fedb874c
david <davidf1212@gmail.com>
2015-05-29 02:03:51 -0400

    libs

---
+++ conf.lua
@@ -0,0 +1,7 @@
+function love.conf(t)
+	t.title = "Title"
+	t.window.width = 800
+	t.window.height = 640
+	t.window.resizable = false
+	t.window.vsync = true
+end
---
+++ main.lua
@@ -0,0 +1,11 @@
+function love.load()
+	math.randomseed(os.time())
+	love.graphics.setDefaultFilter('nearest', 'nearest')
+	love.graphics.setBackgroundColor(45, 45, 45)
+end
+
+function love.update(dt)
+end
+
+function love.draw()
+end
