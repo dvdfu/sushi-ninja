dce88ffc61998c77a94f77d0c9fcab25336f4d09
Seikun Kambashi <seikun_lee@hotmail.com>
2015-05-30 18:44:48 -0400

    collision detection basic

---
+++ mine.lua
@@ -0,0 +1,39 @@
+Class = require 'lib.class'
+Vector = require 'lib.vector'
+
+Mine = Class{
+	init = function(self, id, x, y, player)
+		self.type = "MINE"
+		self.id = id
+		self.pos = Vector(x, y)
+		self.player = player
+
+		self.body = love.physics.newBody(world, self.pos.x, self.pos.y, 'static')
+		self.shape = love.physics.newCircleShape(5)
+		self.fixture = love.physics.newFixture(self.body, self.shape)
+		self.fixture:setUserData(self)
+	end
+}
+
+function Mine:update(dt)
+end
+
+function Mine:draw()
+  love.graphics.setColor(0, 0, 0, 255)
+  love.graphics.circle("fill", self.body:getX(), self.body:getY(), self.shape:getRadius())
+  love.graphics.setColor(255, 255, 255, 255)
+end
+
+function Mine:explode()
+	for key, mine in pairs(self.player.mines) do
+		if key == self.id then
+			table.remove(self.player.mines, key)
+			self.player.minesCount = self.player.minesCount - 1
+			return
+		end
+	end
+end
+
+function Mine:setId(id) self.id = id end
+
+return Mine
