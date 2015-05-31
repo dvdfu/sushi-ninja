7bd0b3497a33154706a5a13eb17a2fa963e2ecb8
david <davidf1212@gmail.com>
2015-05-31 00:09:39 -0400

    sushi

--- coin.lua
+++ coin.lua
@@ -4,6 +4,7 @@ Vector = require 'lib.vector'
 Coin = Class {
 	RADIUS = 5,
 	INI_ID = 1,
+	SUSHI1_SPR = love.graphics.newImage('img/sushi2.png'),
 	init = function(self, pos)
 		self.type = OBJ_TYPE.COIN
 		self.pos = pos
@@ -15,6 +16,9 @@ Coin = Class {
 		self.shape = love.physics.newCircleShape(Coin.RADIUS)
 		self.fixture = love.physics.newFixture(self.body, self.shape)
 		self.fixture:setUserData(self)
+
+
+		Coin.SUSHI1_SPR:setFilter('nearest', 'nearest')
 	end
 }
 
@@ -24,9 +28,10 @@ end
 
 function Coin:draw()
 	-- Set draw to Yellow
-	love.graphics.setColor(255, 255, 0)
-	love.graphics.circle('line', self.pos.x, self.pos.y, Coin.RADIUS, 10)
-	love.graphics.setColor(255, 255, 255)
+	-- love.graphics.setColor(255, 255, 0)
+	-- love.graphics.circle('line', self.pos.x, self.pos.y, Coin.RADIUS, 10)
+	-- love.graphics.setColor(255, 255, 255)
+	love.graphics.draw(Coin.SUSHI1_SPR, self.pos.x, self.pos.y, 0, 2, 2, 16, 16)
 end
 
 function Coin:delete()
